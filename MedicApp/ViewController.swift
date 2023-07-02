dc97e76be5cfe19712ed9b14b525edd8f7614b1a
Airen <airenalfonzo1@gmail.com>
2023-07-02 16:14:34 +0200

    Initial Commit

---
+++ MedicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicApp
+//
+//  Created by Airen Alfonzo on 02/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
