a2b34b147ba13100146ef99f161e94df65f59c5d
Airen <airenalfonzo1@gmail.com>
2023-08-06 18:15:44 +0200

    Class TabBar

---
+++ MedicApp/TabBar/TabBarViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class TabBarViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //investigar como hacer un tabBar
+        //y crear los viewController para cada item
+    }
+}
